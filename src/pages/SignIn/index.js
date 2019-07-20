c0ae957bc6a05d85d761ed3db7548388fe1cacb0
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-07-20 11:43:58 -0300

    Adiciona componente Button e Input

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 export default function SignIn() {
-  return <Background />;
+  return (
+    <Background>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button>Entrar</Button>
+    </Background>
+  );
 }
