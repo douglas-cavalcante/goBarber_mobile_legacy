68d168c1d0cfcc55641da9fed09cd5623a696457
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-07-20 11:13:08 -0300

    Adiciona background component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
 export default function SignIn() {
-  return <Text>teste</Text>;
+  return <Background />;
 }
